3cd22ebd287bc463457e060d69b80e9a1a35cfda
Nguyen Khac Nhat <nhat@agilead.vn>
2018-04-27 22:09:27 +0700

    First commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Simple JavaScript Refactoring (Variable Extraction) example used at [CodeGym](https://codegym.vn)
---
+++ js/fizzbuzz.js
@@ -0,0 +1,13 @@
+var FizzBuzz =  {
+    fizzBuzz: function (number){
+        if(number % 3 == 0 && number % 5 == 0)
+            return "FizzBuzz";
+        if(number % 3 == 0)
+            return "Fizz";
+        if(number % 5 == 0)
+            return "Buzz";
+        return number + "";
+    }
+};
+
+module.exports = FizzBuzz;
